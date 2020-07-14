da4a7fe6b208a9e4831d02dcbf6e6fe3ccb027cc
NCook <nicolas.cook@mayor.cl>
2020-07-14 09:49:35 -0400

    ultimos cambios

---
+++ src/app/components/comofunca/comofunca.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comofunca',
+  templateUrl: './comofunca.component.html',
+  styleUrls: ['./comofunca.component.css']
+})
+export class ComofuncaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
